59e9907edb4b4222da6360bc44c6c34a410cc7a0
Dustin <Dustin@192.168.1.3>
2014-11-05 18:14:41 +0100

    added pre-selection function and all ImageButtons are now working

--- ProjectLeviosa/src/com/example/projectleviosa/MainActivity.java
+++ ProjectLeviosa/src/com/example/projectleviosa/MainActivity.java
@@ -29,35 +29,59 @@ public class MainActivity extends Activity implements OnClickListener {
 		isClicked = 0;
 		indexButton = -1;
 		buttons = new ArrayList<MemoryButton>();
-		buttons.add(new MemoryButton((ImageButton) findViewById(R.id.imageButton1), 0));
-		buttons.add(new MemoryButton((ImageButton) findViewById(R.id.imageButton2), 1));
-		buttons.add(new MemoryButton((ImageButton) findViewById(R.id.imageButton3), 2));
-		buttons.add(new MemoryButton((ImageButton) findViewById(R.id.imageButton4), 3));
-/*		buttons.add(b1 = (ImageButton) findViewById(R.id.imageButton5));
-		buttons.add(b2 = (ImageButton) findViewById(R.id.imageButton6));
-		buttons.add(b3 = (ImageButton) findViewById(R.id.imageButton7));
-		buttons.add(b4 = (ImageButton) findViewById(R.id.imageButton8));
-		buttons.add(c1 = (ImageButton) findViewById(R.id.imageButton9));
-		buttons.add(c2 = (ImageButton) findViewById(R.id.imageButton10));
-		buttons.add(c3 = (ImageButton) findViewById(R.id.imageButton11));
-		buttons.add(c4 = (ImageButton) findViewById(R.id.imageButton12));
-		buttons.add(d1 = (ImageButton) findViewById(R.id.imageButton13));
-		buttons.add(d2 = (ImageButton) findViewById(R.id.imageButton14));
-		buttons.add(d3 = (ImageButton) findViewById(R.id.imageButton15));
-		buttons.add(d4 = (ImageButton) findViewById(R.id.imageButton16));
-		buttons.add(e1 = (ImageButton) findViewById(R.id.imageButton17));
-		buttons.add(e2 = (ImageButton) findViewById(R.id.imageButton18));
-		buttons.add(e3 = (ImageButton) findViewById(R.id.imageButton19));
-		buttons.add(e4 = (ImageButton) findViewById(R.id.imageButton20));
-		buttons.add(f1 = (ImageButton) findViewById(R.id.imageButton21));
-		buttons.add(f2 = (ImageButton) findViewById(R.id.imageButton22));
-		buttons.add(f3 = (ImageButton) findViewById(R.id.imageButton23));
-		buttons.add(f4 = (ImageButton) findViewById(R.id.imageButton24));*/
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton1), 0));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton2), 1));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton3), 2));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton4), 3));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton5), 4));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton6), 5));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton7), 6));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton8), 7));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton9), 8));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton10), 9));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton11), 10));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton12), 11));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton13), 12));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton14), 13));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton15), 14));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton16), 15));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton17), 16));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton18), 17));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton19), 18));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton20), 19));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton21), 20));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton22), 21));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton23), 22));
+		buttons.add(new MemoryButton(
+				(ImageButton) findViewById(R.id.imageButton24), 23));
+
 		restart = (Button) findViewById(R.id.button1);
 		end = (Button) findViewById(R.id.button2);
 
-		for(int i = 0; i < buttons.size(); i++)
-		{
+		for (int i = 0; i < buttons.size(); i++) {
 			buttons.get(i).getButton().setOnClickListener(this);
 		}
 		restart.setOnClickListener(this);
@@ -88,21 +112,77 @@ public class MainActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.imageButton1:
-			
+
 			turnOverCard(buttons.get(0));
-			//indexButton = 0;
 			break;
 		case R.id.imageButton2:
 			turnOverCard(buttons.get(1));
-			//indexButton = 1;
 			break;
 		case R.id.imageButton3:
 			turnOverCard(buttons.get(2));
-			//indexButton = 2;
 			break;
 		case R.id.imageButton4:
 			turnOverCard(buttons.get(3));
-			//indexButton = 3;
+			break;
+		case R.id.imageButton5:
+			turnOverCard(buttons.get(4));
+			break;
+		case R.id.imageButton6:
+			turnOverCard(buttons.get(5));
+			break;
+		case R.id.imageButton7:
+			turnOverCard(buttons.get(6));
+			break;
+		case R.id.imageButton8:
+			turnOverCard(buttons.get(7));
+			break;
+		case R.id.imageButton9:
+			turnOverCard(buttons.get(8));
+			break;
+		case R.id.imageButton10:
+			turnOverCard(buttons.get(9));
+			break;
+		case R.id.imageButton11:
+			turnOverCard(buttons.get(10));
+			break;
+		case R.id.imageButton12:
+			turnOverCard(buttons.get(11));
+			break;
+		case R.id.imageButton13:
+			turnOverCard(buttons.get(12));
+			break;
+		case R.id.imageButton14:
+			turnOverCard(buttons.get(13));
+			break;
+		case R.id.imageButton15:
+			turnOverCard(buttons.get(14));
+			break;
+		case R.id.imageButton16:
+			turnOverCard(buttons.get(15));
+			break;
+		case R.id.imageButton17:
+			turnOverCard(buttons.get(16));
+			break;
+		case R.id.imageButton18:
+			turnOverCard(buttons.get(17));
+			break;
+		case R.id.imageButton19:
+			turnOverCard(buttons.get(18));
+			break;
+		case R.id.imageButton20:
+			turnOverCard(buttons.get(19));
+			break;
+		case R.id.imageButton21:
+			turnOverCard(buttons.get(20));
+			break;
+		case R.id.imageButton22:
+			turnOverCard(buttons.get(21));
+			break;
+		case R.id.imageButton23:
+			turnOverCard(buttons.get(22));
+			break;
+		case R.id.imageButton24:
+			turnOverCard(buttons.get(23));
 			break;
 
 		case R.id.button1:
@@ -115,18 +195,45 @@ public class MainActivity extends Activity implements OnClickListener {
 	}
 
 	private void turnOverCard(MemoryButton button) {
-		isClicked += 1;
-		
-		if (isClicked == 3) {
-			for(int i = 0; i < buttons.size(); i++)
-			{
-				buttons.get(i).getButton().setImageResource(R.drawable.ic_launcher);
+		if (button.isSelected && (!button.isLocked)) {
+			isClicked += 1;
+			if (isClicked == 3) {
+				for (int i = 0; i < buttons.size(); i++) {
+					buttons.get(i).getButton()
+							.setImageResource(R.drawable.ic_launcher);
+					buttons.get(i).setIsTurned(false);
+				}
+				isClicked = 1;
 			}
-			isClicked = 1;
-		}
-		button.getButton().setImageResource(R.drawable.levizoa);
-		button.setIsTurned(true);
+			button.getButton().setImageResource(R.drawable.levizoa);
+			button.setIsSelected(false);
+			button.setIsTurned(true);
+			// Sprachausgabe: "Ein xyz!"
 
+			if (isClicked == 2) { // Jetzt muss hier geprueft werden, ob die
+									// Partnerkarte gefunden worden ist
+				checkIfPair(button);
+
+			}
+		} else if ((!button.isSelected) && (!button.isLocked)) {
+			deleteSelections();
+			button.setIsSelected(true);
+			// Sprachausgabe: "A1"
+		} else if (button.isLocked = true) {
+			// Tue nichts, optional Sprachausgabe:
+			// "Diese Karte hat Ihren Partner bereits gefunden! ect."
+		}
+
+	}
+
+	private void deleteSelections() {
+		for (int i = 0; i < buttons.size(); i++) {
+			buttons.get(i).setIsSelected(false);
+		}
+	}
+
+	private void checkIfPair(MemoryButton button) {
+		//Implementierung folgt, vorher muessen verschiedene Bilder mit jeweiligen IDs gespeichert werden
 	}
 
 }
--- ProjectLeviosa/src/com/example/projectleviosa/MemoryButton.java
+++ ProjectLeviosa/src/com/example/projectleviosa/MemoryButton.java
@@ -5,22 +5,31 @@ import android.widget.ImageButton;
 public class MemoryButton {
 
 	ImageButton button;
-	boolean isTurned;
 	int id;
+	boolean isSelected;
+	boolean isTurned;
 	boolean isLocked;
-	
+
 	MemoryButton(ImageButton buttonP, int idP) {
 		if (idP >= 0) {
 			id = idP;
-		}
-		else {
+		} else {
 			throw new IllegalArgumentException();
 		}
-		button = buttonP;
+		if (buttonP != null) {
+			button = buttonP;
+		} else {
+			throw new IllegalArgumentException();
+		}
+		isSelected = false;
 		isTurned = false;
 		isLocked = false;
 	}
-	
+
+	public void setIsSelected(boolean value) {
+		isSelected = value;
+	}
+
 	public void setIsTurned(boolean value) {
 		isTurned = value;
 	}
@@ -33,20 +42,20 @@ public class MemoryButton {
 		return button;
 	}
 
+	public boolean isSelected() {
+		return isSelected;
+	}
 
 	public boolean isTurned() {
 		return isTurned;
 	}
-	
+
 	public boolean isLocked() {
 		return isLocked;
 	}
-
 
 	public int getId() {
 		return id;
 	}
 
-	
-	
 }
